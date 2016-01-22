ce2ed62823320d5f5553a5bc6057a225547becd9
tao.xing@qq.com <tao.xing@qq.com>
2016-01-22 22:14:56 +0800

    添加了关于精度的判断

--- testsqrt/mysqrt.py
+++ testsqrt/mysqrt.py
@@ -2,9 +2,17 @@
 from numpy import *
 x = 2.0
 s = 1.0
+kmax = 100
+tol = 1.e-14
 
-for k in range(6):
-    print "第",k,"次迭代之前s的值是",s
+for k in range(kmax):
+    
+    s0 = s
     s = 0.5*(s + x/s)
     print "第",k,"次迭代之后s的值是",s
+    delta_s = s - s0
+    if abs(delta_s / x) < tol:
+        break
+             
+print k,"次迭代之后s的值是",s
 
