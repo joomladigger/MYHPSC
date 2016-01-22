80d9d220d25033a5f514597ac588596675222458
tao.xing@qq.com <tao.xing@qq.com>
2016-01-22 22:22:32 +0800

    转换为可以调用的函数

--- testsqrt/mysqrt.py
+++ testsqrt/mysqrt.py
@@ -1,18 +1,18 @@
 #coding:UTF-8
 from numpy import *
-x = 2.0
-s = 1.0
-kmax = 100
-tol = 1.e-14
-
-for k in range(kmax):
+def sqrt2(x):
+    s = 1.0
+    kmax = 100
+    tol = 1.e-14
     
-    s0 = s
-    s = 0.5*(s + x/s)
-    print "第",k,"次迭代之后s的值是",s
-    delta_s = s - s0
-    if abs(delta_s / x) < tol:
-        break
-             
-print k,"次迭代之后s的值是",s
+    for k in range(kmax):
+        
+        s0 = s
+        s = 0.5*(s + x/s)
+        print "第",k + 1,"次迭代之后s的值是",s
+        delta_s = s - s0
+        if abs(delta_s / x) < tol:
+            break
+                 
+    print k + 1,"次迭代之后s的值是",s
 
